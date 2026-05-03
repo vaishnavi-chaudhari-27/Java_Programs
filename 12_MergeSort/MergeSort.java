bd6b8da65124498d0f70caddbbc96ca6bbb0b097
vaishnavi-chaudhari-27 <vaishnavichaudhari278@gmail.com>
2026-05-03 07:51:58 +0530

    Added 3 files

---
+++ 12_MergeSort/MergeSort.java
@@ -0,0 +1,88 @@
+
+// public class MergeSort {
+//     public static void conquer(int arr[], int si, int mid, int ei) {
+//         int merger[] = new int[ei - si + 1];
+//         int idx1 = si;
+//         int idx2 = mid + 1;
+//         int x = 0;
+
+//         while (idx1 <= mid && idx2 <= ei) {
+//             if (arr[idx1] <= arr[idx2]) {
+//                 merged[x++] = arr[idx1++];
+//             } else {
+//                 merged[x++] = arr[idx2++];
+//             }
+//         }
+//         while (idx1 <= mid) {
+//             merged[x++] = arr[idx1++];
+//         }
+//         while (idx2 <= ei) {
+//             merged[x++] = arr[idx2++];
+//         }
+
+//         for (int i = 0; i < merger.length; i++) {
+//             arr[j] = merger[i];
+//         }
+//     }
+
+//     public static void divide(int arr[], int si, int ei) {
+//         if (si >= ei) {
+//             return;
+//         }
+//         mid = si + (ei - si) / 2;
+//         divide(arr, si, mid);
+//         divide(arr, mid + 1, ei);
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = { 6, 3, 9, 5, 2, 8 };
+
+//         divide(arr, 0, n - 1);
+
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+public class quickSort {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 6, 3, 9, 5, 2, 8 };
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
